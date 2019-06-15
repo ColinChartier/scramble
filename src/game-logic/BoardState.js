cf1b2e9dca3ed4651f1dc2bf5d0620ee34510574
Colin Chartier <colin.n.chartier@gmail.com>
2019-06-15 18:18:57 -0400

    Add tile types.

--- src/game-logic/BoardState.js
+++ src/game-logic/BoardState.js
@@ -15,6 +15,34 @@ export default class BoardState {
     }
 
     tileType(i, j) {
-        return TILE_TYPES['STANDARD']
+        //default: _
+        //double letter: A
+        //triple letter: B
+        //double word: X
+        //triple word: Y
+        const board = [
+            "Y__B___Y___B__Y",
+            "_X___B___B___X_",
+            "__X___A_A___X__",
+            "A__X___A___X__A",
+            "____X_____X____",
+            "_B___B___B___B_",
+            "__A___B_B___A__",
+            "Y__A___X___A__Y",
+            "__A___B_B___A__",
+            "_B___B___B___B_",
+            "____X_____X____",
+            "A__X___A___X__A",
+            "__X___A_A___X__",
+            "_X___B___B___X_",
+            "Y__B___Y___B__Y",
+        ];
+        return {
+            '_': TILE_TYPES['STANDARD'],
+            'A': TILE_TYPES['DOUBLE_LETTER_SCORE'],
+            'B': TILE_TYPES['TRIPLE_LETTER_SCORE'],
+            'X': TILE_TYPES['DOUBLE_WORD_SCORE'],
+            'Y': TILE_TYPES['TRIPLE_WORD_SCORE'],
+        }[board[i].charAt(j)]
     }
 }
