17f6239776bb0c6739dab62cd7ca9b933bf838d0
Mathieu Chartier <mathieu.a.chartier@gmail.com>
2019-06-15 14:11:33 -0700

    Refactor tile types

---
+++ src/game-logic/TileTypes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const TILE_TYPES = {
+    'TRIPLE_WORD_SCORE': {
+        color: 'red',
+        wordMultiplier: 3,
+        letterMultiplier: 1,
+    },
+    'DOUBLE_WORD_SCORE': {
+        color: 'pink',
+        wordMultiplier: 2,
+        letterMultiplier: 1,
+    },
+    'TRIPLE_LETTER_SCORE': {
+        color: 'darkblue',
+        wordMultiplier: 1,
+        letterMultiplier: 3,
+    },
+    'DOUBLE_LETTER_SCORE': {
+        color: 'purple',
+        wordMultiplier: 1,
+        letterMultiplier: 2,
+    },
+    'STANDARD': {
+        color: 'beige',
+        wordMultiplier: 1,
+        letterMultiplier: 1,
+    },
+};
+
+export { TILE_TYPES };
--- src/scramble-board/ScrambleBoard.js
+++ src/scramble-board/ScrambleBoard.js
@@ -1,32 +1,5 @@
 import React from 'react';
-
-const TILE_TYPES = {
-    'TRIPLE_WORD_SCORE': {
-        color: 'red',
-        wordMultiplier: 3,
-        letterMultiplier: 1,
-    },
-    'DOUBLE_WORD_SCORE': {
-        color: 'pink',
-        wordMultiplier: 2,
-        letterMultiplier: 1,
-    },
-    'TRIPLE_LETTER_SCORE': {
-        color: 'darkblue',
-        wordMultiplier: 1,
-        letterMultiplier: 3,
-    },
-    'DOUBLE_LETTER_SCORE': {
-        color: 'purple',
-        wordMultiplier: 1,
-        letterMultiplier: 2,
-    },
-    'STANDARD': {
-        color: 'beige',
-        wordMultiplier: 1,
-        letterMultiplier: 1,
-    },
-};
+import { TILE_TYPES } from '../game-logic/TileTypes';
 
 class Tile extends React.Component {
     render() {
